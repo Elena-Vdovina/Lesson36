5514f9d79d0cf87ca27413b59b19b0ae763f5473
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-09 09:54:59 +0100

    Add constructor, setters, getters

--- src/Triangle.java
+++ src/Triangle.java
@@ -8,7 +8,43 @@ public class Triangle {
 // неравенство треугольника.
 // Этот метод необходимо использовать в конструкторе.
 // При провале любой из проверок нужно вывести на экран соответствующее сообщение.
+
+  private int sideA;
+  private int sideB;
+  private int sideC;
+
+  public Triangle(int sideA, int sideB, int sideC) {
+    if (sideA <= 0 || sideB <= 0 || sideC <= 0) {
+      System.out.println("Не допустимое значение!");
+      System.exit(1);
+    }
+    if (!checkSides(sideA, sideB, sideC)) {
+      System.out.println("Такой треугольник не может существовать либо вырожден!");
+      System.exit(1);
+    }
+  }
+
+  public void setSideA(int sideA) {
+    this.sideA = sideA;
+  }
+
+  public void setSideB(int sideB) {
+    this.sideB = sideB;
+  }
+
+  public void setSideC(int sideC) {
+    this.sideC = sideC;
+  }
+
+  public int getSideA() {
+    return sideA;
+  }
+
+  public int getSideB() {
+    return sideB;
+  }
+
+  public int getSideC() {
+    return sideC;
+  }
 }
-
-
-
